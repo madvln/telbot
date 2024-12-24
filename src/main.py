f68345355b3cbec3f8ef6dc616c558a1fd382043
madvln <madvln@yandex.ru>
2024-12-24 10:48:40 +0000

    ci-cd-1-test

--- src/main.py
+++ src/main.py
@@ -1,14 +1,27 @@
 # main.py
 import os
 from dotenv import load_dotenv
+
+# Загружаем .env, если он есть
 load_dotenv()
+
+# Получаем токен из переменной окружения
+API_TOKEN = os.getenv("TOKEN")
+
+# Проверяем, задан ли токен
+if not API_TOKEN:
+    # Если токен не найден, попробуем загрузить из Secrets GitHub
+    API_TOKEN = os.environ.get("TOKEN")
+    if not API_TOKEN:
+        raise ValueError(
+            "Telegram API token is not set. Please provide it via .env file or as an environment variable."
+        )
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 
 from handlers.mention_handler import mention_handler
 from handlers.welcome_handler import welcome_handler
 from handlers.rule_handler import rule_handler
 
-API_TOKEN = os.getenv("TOKEN")
 
 def main() -> None:
     updater = Updater(API_TOKEN)
@@ -29,6 +42,7 @@ def main() -> None:
     # Запуск бота
     updater.start_polling()
     updater.idle()
+
 
 if __name__ == "__main__":
     main()
